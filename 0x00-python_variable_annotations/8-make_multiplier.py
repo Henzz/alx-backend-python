c88f3013011bf586eac4fe371e80a054110f0f24
Henzz <khenok53@gmail.com>
2024-07-05 05:50:56 +0300

    task 8 done

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to create a multiplier function.
+"""
+
+from typing import Callable, float
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Creates a function that multiplies a float by a given multiplier.
+
+    Args:
+        multiplier (float): The value to multiply by.
+
+    Returns:
+        Callable[[float], float]: A function that takes a float and returns
+        the product of the float and the multiplier.
+    """
+    def multiplier_function(num: float) -> float:
+        """
+        Multiplies a float by the given multiplier.
+
+        Args:
+            num (float): The value to be multiplied.
+
+        Returns:
+            float: The result of multiplying the given number by the
+            multiplier.
+        """
+        return num * multiplier
+    return multiplier_function
