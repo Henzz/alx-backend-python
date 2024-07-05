c97cc1a5cf10895a30cc25ee7108be7dbb609e4c
Henzz <khenok53@gmail.com>
2024-07-05 05:37:24 +0300

    task 2 done

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,7 +2,6 @@
 """
 This module provides a function to add two float numbers.
 """
-from typing import Optional
 
 
 def add(a: float, b: float) -> float:
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to find the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Returns the floor of the given float.
+
+    Args:
+        n (float): The float value to find the floor of.
+
+    Returns:
+        int: The floor of the input float.
+    """
+    return math.floor(n)
