b9fb6670cf1ca4945a0ffd26aeb9be52fa736d80
Henzz <khenok53@gmail.com>
2024-07-06 02:15:55 +0300

    task 11 pystyle fix

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -8,15 +8,19 @@ from typing import Mapping, Any, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None] = None) -> Union[Any, T]:
+def safely_get_value(
+        dct: Mapping, key: Any,
+        default: Union[T, None] = None) -> Union[Any, T]:
     """
     Safely get the value from a dictionary for a given key.
 
     If the key is found in the dictionary, the value is returned.
-    Otherwise, the default value (or None if no default is provided) is returned.
+    Otherwise, the default value (or None if no default is
+    provided) is returned.
 
     Returns:
-        Union[Any, T]: The value from the dictionary, or the default value if the key is not found.
+        Union[Any, T]: The value from the dictionary, or the
+        default value if the key is not found.
     """
     if key in dct:
         return dct[key]
