6931da4118055fe58414da87d81c504dc86727eb
Henzz <khenok53@gmail.com>
2024-07-05 05:29:14 +0300

    task 0 done and gitignore added

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to add two float numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers and returns the result as a float
+    """
+    return a + b
