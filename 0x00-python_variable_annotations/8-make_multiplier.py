13e41691dcc400d885c2ab1077590b5a439d928e
Henzz <khenok53@gmail.com>
2024-07-05 05:52:35 +0300

    task 8 fix

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -3,7 +3,7 @@
 This module provides a function to create a multiplier function.
 """
 
-from typing import Callable, float
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
