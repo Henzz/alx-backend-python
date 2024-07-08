e7431bf732b9fafb06768e5ab77a7dc9ad298587
Henzz <khenok53@gmail.com>
2024-07-09 01:47:23 +0300

    task 4 fix

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -6,9 +6,11 @@ This module calls task_wait_random.
 import asyncio
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> list[float]:
     """
-    Creates n tasks with task_wait_random(max_delay) and waits for them all to complete.
+    Creates n tasks with task_wait_random(max_delay) and waits
+    for them all to complete.
 
     Args:
         n (int): The number of tasks to create.
@@ -17,9 +19,6 @@ async def task_wait_n(n: int, max_delay: int) -> list[float]:
     Returns:
         list[float]: The list of delays (in seconds) for each completed task.
     """
-    delays = []
     tasks = [task_wait_random(max_delay) for _ in range(n)]
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-    return delays
+    delays = await asyncio.gather(*tasks)
+    return sorted(delays)
