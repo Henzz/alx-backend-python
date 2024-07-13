88ff8e2ed19bd2b3eb018280cea17ec68584ba7d
Henzz <khenok53@gmail.com>
2024-07-13 05:06:49 +0300

    task 0 asyn comprehension fix

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -16,6 +16,6 @@ async def async_generator() -> int:
     Return:
         random number between 0 and 10
     """
-    async for i in 10:
-        yield i
+    for _ in range(10):
         await asyncio.sleep(1)
+        yield random.uniform(0, 10)
